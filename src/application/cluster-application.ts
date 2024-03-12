df779bee672ea1c023a9af55509ccbd283035ed7
Chiro Hiro <chiro8x@gmail.com>
2024-03-11 22:07:47 -0700

    Fix cluster application ESM issue

--- src/application/cluster-application.ts
+++ src/application/cluster-application.ts
@@ -1,16 +1,24 @@
 import cluster, { Worker } from 'cluster';
 import EventEmitter from 'events';
 
-export interface IApplicationPayload {
-  name: string;
-  payload: string;
-  [key: string]: string;
-}
+// Application type, payload is a function that returns a Promise<number>
+export type Application = { payload: () => Promise<number> };
 
+// ApplicationMetadata type, clusterName is a string
+export type ApplicationMetadata = { clusterName: string };
+
+/**
+ * ApplicationPayload type
+ * This type is a combination of Application and ApplicationMetadata
+ */
+export type ApplicationPayload = Application & ApplicationMetadata;
+
+// ClusterApplication class
+// This class is used to manage the cluster workers and their payloads
 export class ClusterApplication extends EventEmitter {
-  private workerStorage: Map<string, IApplicationPayload> = new Map();
+  private workerMap: Map<string, ApplicationPayload> = new Map();
 
-  private workerMap: Map<number, IApplicationPayload> = new Map();
+  private pidMap: Map<number, string> = new Map();
 
   private shutdown: boolean = false;
 
@@ -21,20 +29,20 @@ export class ClusterApplication extends EventEmitter {
     this.autoRestart = restartPolicy;
   }
 
-  public add(environment: IApplicationPayload): ClusterApplication {
+  public add(app: ApplicationPayload): ClusterApplication {
     if (cluster.isPrimary) {
-      this.emit('new', environment);
+      this.emit('new', app.clusterName);
     }
 
-    this.workerStorage.set(environment.name, environment);
+    this.workerMap.set(app.clusterName, app);
     return this;
   }
 
-  private restartWorker(pid: number, e?: IApplicationPayload) {
-    if (pid < 0 && typeof e !== 'undefined') {
-      const curWorker = cluster.fork(e);
+  private restartWorker(pid: number, app?: ApplicationPayload) {
+    if (pid < 0 && typeof app !== 'undefined') {
+      const curWorker = cluster.fork({ clusterName: app.clusterName });
       if (typeof curWorker.process.pid !== 'undefined') {
-        this.workerMap.set(curWorker.process.pid, e);
+        this.pidMap.set(curWorker.process.pid, app.clusterName);
       } else {
         this.emit(
           'error',
@@ -43,30 +51,28 @@ export class ClusterApplication extends EventEmitter {
       }
       return;
     }
-    if (this.workerMap.has(pid)) {
-      const environment = this.workerMap.get(pid);
-      if (environment) {
-        const curWorker = cluster.fork(environment);
-        if (typeof curWorker.process.pid !== 'undefined') {
-          this.workerMap.set(curWorker.process.pid, environment);
-          this.emit('restart', curWorker.process.pid, environment);
-          this.workerMap.delete(pid);
-        } else {
-          this.emit(
-            'error',
-            new Error('ClusterApplication::restartWorker() invalid process id')
-          );
-        }
-        return;
+    if (this.pidMap.has(pid)) {
+      const clusterName = this.pidMap.get(pid)!;
+      const curWorker = cluster.fork({ clusterName });
+      if (typeof curWorker.process.pid !== 'undefined') {
+        this.pidMap.set(curWorker.process.pid, clusterName);
+        this.emit('restart', curWorker.process.pid, clusterName);
+        this.pidMap.delete(pid);
+      } else {
+        this.emit(
+          'error',
+          new Error('ClusterApplication::restartWorker() invalid process id')
+        );
       }
+      return;
     }
     this.emit('error', new Error('Pid or payload is invalid'));
   }
 
   private startWorkers() {
-    this.workerStorage.forEach((e: IApplicationPayload) => {
-      this.restartWorker(-1, e);
-    });
+    for (let appPayload of this.workerMap.values()) {
+      this.restartWorker(-1, appPayload);
+    }
   }
 
   public start() {
@@ -123,8 +129,25 @@ export class ClusterApplication extends EventEmitter {
       process.on('SIGTERM', handler);
       process.on('SIGINT', handler);
     } else {
-      const { payload } = process.env;
-      import(payload || '');
+      const { clusterName } = process.env;
+      if (typeof clusterName !== 'undefined') {
+        const appPayload = this.workerMap.get(clusterName);
+        if (typeof appPayload !== 'undefined') {
+          appPayload.payload().then((code) => {
+            this.emit('exit', code);
+          });
+        } else {
+          this.emit(
+            'error',
+            new Error('ClusterApplication::start() invalid payload')
+          );
+        }
+      } else {
+        this.emit(
+          'error',
+          new Error('ClusterApplication::start() invalid cluster name')
+        );
+      }
     }
   }
 }
